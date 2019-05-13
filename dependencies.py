f0733f07cbebeaf38abc2d799cd5425ae13dcfb1
benjaminaudet <benjamin.audet@epitech.eu>
2019-05-13 17:21:31 +0300

    clean dependencies script

--- dependencies.py
+++ dependencies.py
@@ -4,15 +4,6 @@ pip_install_argument = "install"
 packages_to_install = ['bimdata-api-client', 'matplotlib', 'numpy', 'oic', 'pandas', 'requests']
 
 def install(packages):
-    """installes given packages via pip
-
-    Args:
-        package names as list
-
-    Returns:
-        None
-
-    """
     global pip_install_argument
     for package in packages:
         pip([pip_install_argument, package])
