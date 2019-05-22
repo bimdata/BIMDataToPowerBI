eb8f4e167c1d07b4642ee070e9a3440968bd72e1
benjaminaudet <benjamin.audet@epitech.eu>
2019-05-22 11:44:35 +0200

    Fix naming

--- dependencies.py
+++ dependencies.py
@@ -1,7 +1,7 @@
 from pip._internal import main as pip
 
 pip_install_argument = "install"
-packages_to_install = ['bimdata-api-client', 'matplotlib', 'numpy', 'oic', 'pandas', 'requests']
+packages_to_install = ['bimdata_api_client', 'matplotlib', 'numpy', 'oic', 'pandas', 'requests']
 
 def install(packages):
     global pip_install_argument
