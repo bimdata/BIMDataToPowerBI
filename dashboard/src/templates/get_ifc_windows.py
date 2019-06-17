90ef559ad3c1aa05420622cdfa5ecd9e098f3cf1
benjaminaudet <benjamin.audet@epitech.eu>
2019-06-17 11:28:01 +0200

    Add templates and update dashboard

---
+++ dashboard/src/templates/get_ifc_windows.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3.7
+
+import pandas as pd
+import bimdata_api_client
+import sys
+import json
+import logging
+
+import pprint
+pp = pprint.PrettyPrinter(indent=2, width=120)
+
+class GetElements:
+    def __init__(self, dataset=None, ifc_type=None, debug=False, **kwargs):
+        self.ifc_type = ifc_type
+        self.debug = debug or 'nodebug'
+        self.elements = {}
+        self.flat_elements = {}
+        self.property_names = []
+        if dataset is not None:
+            self.access_token = dataset.iloc[0, 0]
+        else:
+            with open('.env', 'r') as env:
+                self.access_token = env.read()
+
+    def config(self):
+        configuration = bimdata_api_client.Configuration()
+        configuration.api_key['Authorization'] = self.access_token
+        configuration.api_key_prefix['Authorization'] = 'Bearer'
+        configuration.host = 'https://api-staging.bimdata.io'
+        return configuration
+
+    def debug_data(self, data, function_name='MISSING_FUNCTION_NAME'):
+        if 'nodebug' in self.debug:
+            return
+        print('====== DEBUG IN {} ======'.format(function_name))
+        if 'soft' in self.debug:
+            print('Found {} elements in the'.format(len(data), type(data)))
+        elif 'hard' in self.debug:
+            pp.pprint(data)
+
+    def get_all_properties_name(self):
+        for element in self.elements:
+            for pset in element['property_sets']:
+                for prop in pset['properties']:
+                    if prop['definition']['name'] not in self.property_names:
+                        self.property_names.append(prop['definition']['name'])
+
+    def get_properties_from_elements(self):
+        self.get_all_properties_name()
+        for element in self.elements:
+            element['properties'] = []
+            for prop_name in self.property_names:
+                prop = {'value': '', 'name': prop_name}
+                element['properties'].append(prop)
+        for element in self.elements:
+            for pset in element['property_sets']:
+                for prop in pset['properties']:
+                    for prop_target in element['properties']:
+                        if prop_target['name'] == prop['definition']['name']:
+                            prop_target['value'] = prop['value'] if prop['value'] not in ['', None] else ''
+
+    def format_properties_for_power_bi(self):
+        for prop_name in self.property_names:
+            if prop_name not in self.flat_elements:
+                self.flat_elements[prop_name] = []
+        for element in self.elements:
+            for prop in element['properties']:
+                self.flat_elements[prop['name']].append(prop['value'])
+
+    def remove_useless_properties(self):
+        max = len(self.flat_elements['uuid'])
+        list_of_key_to_delete = []
+        for key in self.flat_elements.keys():
+            if len(self.flat_elements[key]) > max:
+                list_of_key_to_delete.append(key)
+        for key_to_delete in list_of_key_to_delete:
+            del self.flat_elements[key_to_delete]
+
+    def equalize_properties(self):
+        self.remove_useless_properties()
+
+    def raw_elements_to_elements(self, api_response):
+        raw_elements = api_response.to_dict()
+
+        for definition in raw_elements['definitions']:
+            definition['unit'] = raw_elements['units'][definition['unit_id']] if definition['unit_id'] else None
+            del definition['unit_id']
+
+        for pset in raw_elements['property_sets']:
+            for prop in pset['properties']:
+                prop['definition'] = raw_elements['definitions'][prop['def_id']]
+                del prop['def_id']
+
+        for elem in raw_elements['elements']:
+            elem['attributes'] = raw_elements['property_sets'][elem['attributes']]
+            elem['classifications'] = list(map(lambda class_id: raw_elements['classifications'][class_id], elem['classifications']))
+            elem['property_sets'] = list(map(lambda pset_id: raw_elements['property_sets'][pset_id], elem['psets']))
+            del elem['psets']
+            self.elements[elem['uuid']] = elem
+
+    def filter_by_types(self):
+        if self.ifc_type:
+            self.elements = [elem for elem in self.elements.values() if elem['type'] == self.ifc_type]
+            return
+        self.elements = [elem for elem in self.elements.values()]
+
+    def run(self):
+        configuration = self.config()
+        ifc_api = bimdata_api_client.IfcApi(bimdata_api_client.ApiClient(configuration))
+        cloud_pk = '305'
+        project_pk = '403'
+        ifc_pk = '803'
+
+        try:
+            api_response = ifc_api.get_raw_elements(cloud_pk, ifc_pk, project_pk)
+            self.raw_elements_to_elements(api_response)
+            self.filter_by_types()
+            self.flat_elements = {}
+            self.flat_elements['uuid'] = [elem['uuid'] for elem in self.elements]
+            self.flat_elements['type'] = [elem['type'] for elem in self.elements]
+            self.get_properties_from_elements()
+            self.format_properties_for_power_bi()
+            self.debug_data(self.flat_elements, sys._getframe().f_code.co_name)
+            return pd.DataFrame(self.flat_elements)
+        except:
+            raise Exception("An error occured during data retrieving, try to refresh the token with the request BIMDataMicrosoftConnect.RefreshToken()")
+
+if __name__ == '__main__':
+    if 'dataset' in globals():
+        get_elements = GetElements(dataset=dataset, ifc_type='IfcWindow')
+    else:
+        get_elements = GetElements(ifc_type='IfcWindow', debug='soft')
+    print("Getting elements from API...")
+    IfcWindows = get_elements.run()
+    print("Finished")
